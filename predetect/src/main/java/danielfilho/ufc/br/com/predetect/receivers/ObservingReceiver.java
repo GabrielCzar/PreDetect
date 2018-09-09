50ee4e420688057a1ce8558c19bb9e8d8c12d78d
Gabriel César <gcal1@live.com>
2018-09-09 02:59:24 -0300

    code improvement

--- predetect/src/main/java/danielfilho/ufc/br/com/predetect/receivers/ObservingReceiver.java
+++ predetect/src/main/java/danielfilho/ufc/br/com/predetect/receivers/ObservingReceiver.java
@@ -7,12 +7,14 @@ import android.util.Log;
 
 import com.elvishew.xlog.LogLevel;
 import com.elvishew.xlog.XLog;
-import com.elvishew.xlog.printer.Printer;
 import com.elvishew.xlog.printer.file.FilePrinter;
 
-
-import danielfilho.ufc.br.com.predetect.constants.PredectConstants;
 import danielfilho.ufc.br.com.predetect.services.NetworkObserverService;
+
+import static danielfilho.ufc.br.com.predetect.constants.PreDetectConstants.ACTION_SERVICE_WAKEFUL_BROADCAST;
+import static danielfilho.ufc.br.com.predetect.constants.PreDetectConstants.LOG_PATH;
+import static danielfilho.ufc.br.com.predetect.constants.PreDetectConstants.LOG_TAG;
+import static danielfilho.ufc.br.com.predetect.constants.PreDetectConstants.WIFI_BUNDLE;
 
 /**
  * Created by Daniel Filho on 11/17/16.
@@ -20,26 +22,25 @@ import danielfilho.ufc.br.com.predetect.services.NetworkObserverService;
 
 public class ObservingReceiver extends WakefulBroadcastReceiver {
 
-
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        if(intent.getAction().equals(PredectConstants.ACTION_SERVICE_WAKEFUL_BROADCAST)) {
-            try {
-                XLog.init(LogLevel.ALL, new FilePrinter.Builder(PredectConstants.LOG_PATH).build());
-            } catch (Exception e) {
-            }
+        if (intent.getAction() == null)
+            return;
 
-            if (intent.getByteArrayExtra(PredectConstants.WIFI_BUNDLE) != null) {
-                XLog.d(System.currentTimeMillis()+"|  -------------- PREDECT : ON BROADCAST  -----------");
-                Log.d(PredectConstants.LOG_TAG, "-------------- PREDECT : ON BROADCAST  -----------");
+        if (intent.getAction().equals(ACTION_SERVICE_WAKEFUL_BROADCAST)) {
+            XLog.init(LogLevel.ALL, new FilePrinter.Builder(LOG_PATH).build());
+
+            if (intent.getByteArrayExtra(WIFI_BUNDLE) != null) {
+                XLog.d(System.currentTimeMillis() + "|  -------------- PRE DETECT : ON BROADCAST  -----------");
+                Log.d(LOG_TAG, "-------------- PRE DETECT : ON BROADCAST  -----------");
                 Intent observingIntent = new Intent(context, NetworkObserverService.class);
-                observingIntent.putExtra(PredectConstants.WIFI_BUNDLE, intent.getByteArrayExtra(PredectConstants.WIFI_BUNDLE));
+                observingIntent.putExtra(WIFI_BUNDLE, intent.getByteArrayExtra(WIFI_BUNDLE));
                 startWakefulService(context, observingIntent);
             } else {
-                Log.e(PredectConstants.LOG_TAG, "-------------- THE BUNDLE ON WIFI OBSERVING IS NULL -----------");
-                XLog.e(System.currentTimeMillis()+"|  -------------- THE BUNDLE ON WIFI OBSERVING IS NULL -----------");
+                Log.e(LOG_TAG, "-------------- THE BUNDLE ON WIFI OBSERVING IS NULL -----------");
+                XLog.e(System.currentTimeMillis() + "|  -------------- THE BUNDLE ON WIFI OBSERVING IS NULL -----------");
             }
         }
+
     }
 }
